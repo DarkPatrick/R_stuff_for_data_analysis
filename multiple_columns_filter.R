8d3fdb14511f10d5675e2e33d48dc5688d07f7e7
DarkPatrick <egorsiomin@yandex.ru>
2018-09-22 11:56:57 +0300

    filter several columns with similar condition

---
+++ multiple_columns_filter.R
@@ -0,0 +1,15 @@
+multiple_filter <- function(data, col_names_pattern, condition) {
+    require(dplyr)
+    require(tidyr)
+
+    col_nums <- grep(col_names_pattern, names(data))
+    filtering_cols <- sapply(col_nums,
+        function(x) {
+            paste0(deparse(substitute(data)), "[", x, "]", condition)
+        }
+    )
+    filtering_str <- paste(filtering_cols, collapse = " & ")
+    ret_val <- data %>% filter(eval(parse(text = filtering_str)))
+
+    return (ret_val)
+}
